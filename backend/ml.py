d954b8324093e41a373b0e87e57294ea92477be7
Zelia Mauro <zeliamauro@Zelias-MacBook-Air.local>
2023-08-17 17:22:47 -0500

    adding clean_crime_data

--- backend/ml.py
+++ backend/ml.py
@@ -1,17 +1,16 @@
 import pandas as pd
 
+input_filename = 'crimes_adjusted.csv'
+output_filename = 'clean_crimes.csv'
+columns_to_remove = [0]  # List of column indices to be removed
+
 # Read the CSV file into a DataFrame
-csv_file_path = 'crashes.csv'
-data = pd.read_csv(csv_file_path)
+data = pd.read_csv(input_filename)
 
-# Identify rows to remove based on a specific condition (for example, removing rows where a column value is below a threshold)
-condition = data["L"].isnull()
+# Drop the specified columns
+data = data.drop(data.columns[columns_to_remove], axis=1)
 
-# Filter out the rows that meet the condition
-data_filtered = data[~condition]
-
-# Write the modified data back to the CSV file
-new_csv_file_path = 'modified_file.csv'
-data_filtered.to_csv(new_csv_file_path, index=False)
+# Write the modified data back to a new CSV file
+data.to_csv(output_filename, index=False)
 
 print("hello")
