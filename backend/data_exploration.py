e14389a10166d45f9872f534f229d933f1adf300
gharris7 <96964444+gharris7@users.noreply.github.com>
2023-08-17 18:38:56 -0500

    added figs + data exploration

--- backend/data_exploration.py
+++ backend/data_exploration.py
@@ -4,10 +4,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load the data
-data = pd.read_csv('/Users/grantharris/repos/Predictor/backend/clean_crash_data.csv')
+crashes = pd.read_csv('~/repos/Predictor/backend/crash.csv')
+crimes = pd.read_csv('~/repos/Predictor/backend/crime.csv')
 
 # Count the frequency of each crash hour
-crash_hour_counts = data['CRASH_HOUR'].value_counts()
+crash_hour_counts = crashes['CRASH_HOUR'].value_counts()
 
 # Sort by the index (crash hour) for a more meaningful chart
 crash_hour_counts = crash_hour_counts.sort_index()
@@ -18,4 +19,18 @@ plt.bar(crash_hour_counts.index, crash_hour_counts.values, color='b')
 plt.xlabel('Crash Hour')
 plt.ylabel('Frequency')
 plt.title('Frequency of Crashes by Hour')
+plt.show()
+
+# Count the frequency of each crime hour
+crime_hour_counts = crimes['crime_hour'].value_counts()
+
+# Sort by the index (crime hour) for a more meaningful chart
+crime_hour_counts = crime_hour_counts.sort_index()
+
+# Create the bar chart
+plt.figure(figsize=(10,6))
+plt.bar(crime_hour_counts.index, crime_hour_counts.values, color='b')
+plt.xlabel('Crime Hour')
+plt.ylabel('Frequency')
+plt.title('Frequency of Crimes by Hour')
 plt.show()
