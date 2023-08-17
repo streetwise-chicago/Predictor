ee9db4c06bb96ca8f6927c8fc714e0ab8549b422
gharris7 <96964444+gharris7@users.noreply.github.com>
2023-08-17 13:56:54 -0500

    load data + cleaning

--- backend/ml.py
+++ backend/ml.py
@@ -1,4 +1,18 @@
 # tba
 
 import torch
-import pandas as pandas
+import pandas as pd
+import os
+from datetime import *
+
+# Crash data (driving)
+
+traffic_raw = pd.read_csv("~/Downloads/traffic_crashes.csv")
+traffic_clean = traffic_raw[["LONGITUDE", "LATITUDE", "WEATHER_CONDITION", "CRASH_HOUR"]]
+print(traffic_clean.head())
+
+# Crime data (walking)
+
+crimes_raw = pd.read_csv("~/Downloads/crimes.csv")
+crimes_clean = crimes_raw[["Latitude", "Longitude", "Date"]]
+crimes_clean.dropna(axis=0, inplace=True)
