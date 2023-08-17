b648b36f628e8bac97a1d1de832a90d8f63241c4
Zelia Mauro <zeliamauro@Zelias-MacBook-Air.local>
2023-08-17 15:51:41 -0500

    Importing the clean crash data

--- backend/ml.py
+++ backend/ml.py
@@ -1,4 +1,19 @@
 # tba
 
-import torch
-import pandas as pandas
+import pandas as pd
+
+# Read the CSV file into a DataFrame
+csv_file_path = 'crashes.csv'
+data = pd.read_csv(csv_file_path)
+
+# Identify rows to remove based on a specific condition (for example, removing rows where a column value is below a threshold)
+condition = data["L"].isnull()
+
+# Filter out the rows that meet the condition
+data_filtered = data[~condition]
+
+# Write the modified data back to the CSV file
+new_csv_file_path = 'modified_file.csv'
+data_filtered.to_csv(new_csv_file_path, index=False)
+
+print("hello")
