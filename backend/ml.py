185198ba695009256a10b5a0456f31e4f0c589d4
Zelia Mauro <zeliamauro@Zelias-MacBook-Air.local>
2023-08-17 18:20:09 -0500

    Adjusted crash and crime datasets to have only 10,000 rows

--- backend/ml.py
+++ backend/ml.py
@@ -1,16 +1,14 @@
 import pandas as pd
 
-input_filename = 'crimes_adjusted.csv'
-output_filename = 'clean_crimes.csv'
-columns_to_remove = [0]  # List of column indices to be removed
+# Read the entire CSV file
+input_file = 'clean_crime_data.csv'
+df = pd.read_csv(input_file)
 
-# Read the CSV file into a DataFrame
-data = pd.read_csv(input_filename)
+# Keep only the first 10,000 rows
+df = df.head(10000)
 
-# Drop the specified columns
-data = data.drop(data.columns[columns_to_remove], axis=1)
-
-# Write the modified data back to a new CSV file
-data.to_csv(output_filename, index=False)
+# Write the first 10,000 rows to a new CSV file
+output_file = 'crime.csv'
+df.to_csv(output_file, index=False)
 
 print("hello")
