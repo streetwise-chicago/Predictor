dd526ebbbc82c41d4cd83e0aae4805278c470446
gharris7 <96964444+gharris7@users.noreply.github.com>
2023-08-17 19:37:18 -0500

    crash dataset update

--- backend/data_exploration.py
+++ backend/data_exploration.py
@@ -27,10 +27,19 @@ crime_hour_counts = crimes['crime_hour'].value_counts()
 # Sort by the index (crime hour) for a more meaningful chart
 crime_hour_counts = crime_hour_counts.sort_index()
 
-# Create the bar chart
+# Create a bar chart
 plt.figure(figsize=(10,6))
 plt.bar(crime_hour_counts.index, crime_hour_counts.values, color='b')
 plt.xlabel('Crime Hour')
 plt.ylabel('Frequency')
 plt.title('Frequency of Crimes by Hour')
 plt.show()
+
+# Create a scatter plot for each
+plt.figure(figsize=(10,6))
+plt.scatter(crashes['LONGITUDE'], crashes['LATITUDE'], alpha=0.5)
+plt.xlabel('Longitude')
+plt.ylabel('Latitude')
+plt.title('Scatter plot of crashes')
+plt.show()
+
